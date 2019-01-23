91673025cdf4819fdcac780fed7afba92f364c01
Gustavo Garzaki <gusgarzaki@gmail.com>
2019-01-23 16:48:09 -0500

    create angular project

---
+++ ui/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { AppComponent } from './app.component'
+import { SignupModule } from './signup/signup.module'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    SignupModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
