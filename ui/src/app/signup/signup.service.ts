41cc85579165b6d57192b10079f69d6386f470fd
Edinson Emilio Nuñez More <enunez@belatrix.com>
2019-01-27 17:50:08 -0500

    add a method to register an user

--- ui/src/app/signup/signup.service.ts
+++ ui/src/app/signup/signup.service.ts
@@ -12,11 +12,16 @@ export class SignupService {
 
   constructor(private apollo: Apollo) { }
 
+  /**
+   * Search an user by his email address
+   * @param email | string user's email who's looking for
+   * @returns boolean if the user exists or not
+   */
   public checkForExists(email: string): Observable<boolean> {
     return Observable.create((sub: Subscriber<boolean>) => {
       this.apollo.query({
         query: gql`
-          query FindUser($email: String!) {
+          query Find($email: String!) {
             user(email: $email) {
               id
             }
@@ -27,6 +32,32 @@ export class SignupService {
       .subscribe((value: ApolloQueryResult<any>) => {
         const found: User | null = value.data.user
         sub.next(found !== null)
+        sub.complete()
+      })
+    })
+  }
+
+  /**
+   * 
+   * @param data | User information of the user
+   * @returns User the recently created user
+   */
+  public register(data: User): Observable<User> {
+    return Observable.create((sub: Subscriber<User>) => {
+      this.apollo.mutate({
+        mutation: gql`
+          mutation Register($data: UserInput!) {
+            createUser(data: $data) {
+              id,
+              email
+            }
+          }
+        `,
+        variables: { data }
+      })
+      .subscribe((value: ApolloQueryResult<any>) => {
+        const created: User = value.data.createUser
+        sub.next(created)
         sub.complete()
       })
     })
